eff46dbbfc006144bfa56871c37d7b112e8d46fd
Chris-846 <yy846@yonsei.ac.kr>
2026-06-28 05:25:52 +0900

    Update tinyatt.py

--- tinyatt.py
+++ tinyatt.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from dataset import vocab_size, block_size
+from selfatt import SingleHeadSelfAttention
 
 class TinyAttentionLM(nn.Module):
     def __init__(self, vocab_size, block_size, emb_dim=64):
